e6ff8219a4a2ac0d1c67998d3303feb8c8aa4b1a
Aron Jones <aron.jones@gmail.com>
2017-05-29 16:43:24 -0400

    latest changes

--- .bashrc
+++ .bashrc
@@ -36,3 +36,4 @@ export NVM_DIR="/home/defrex/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
 export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+
--- .bashrc.d/alias.sh
+++ .bashrc.d/alias.sh
@@ -1,7 +1,37 @@
 
 function mk(){ mkdir -p `dirname $1` && touch $1; }
-function mksb(){ mk $1 && subl $1; }
-function pcat (){ pygmentize "$@" 2>/dev/null || cat "$@"; }
+function mksb(){ mk $1; subl3 $1; }
+function pcat(){ pygmentize "$@" 2>/dev/null | cat --number || cat --number "$@"; }
+
+function dc-rspec(){
+    app=$1
+    shift
+    docker-compose run $app bundle exec rspec $*
+}
+
+function dc-rake(){
+    app=$1
+    shift
+    docker-compose run $app bundle exec rake $*
+}
+
+function dc-rails(){
+    app=$1
+    shift
+    docker-compose run $app bundle exec rails $*
+}
+
+function dc-npm(){
+    app=$1
+    shift
+    docker-compose run $app npm $*
+}
+
+function dc-logs(){
+    app=$1
+    shift
+    docker-compose logs $app -f --tail=100 $*
+}
 
 alias ls='ls --color --human-readable'
 alias gst='git status'
@@ -16,5 +46,6 @@ alias cap='bundle exec cap'
 alias spring='bundle exec spring'
 alias rspec='bundle exec rspec'
 alias git-trim='git checkout master && git branch --merged | grep -v "\*" | grep -v master | xargs -n 1 git branch -d'
+alias dc-run='docker-compose run'
 
 command -v ack-grep >/dev/null 2>&1 && alias ack='ack-grep'
